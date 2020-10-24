5642a22213c147330d7627deab02ad72bafda596
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-23 21:26:57 -0600

    Create nav buttons for home and about views and add link to title #20

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,15 +5,27 @@ import metatronsSettings from '../../assets/metatronsSettings.png'
 const Header = () => {
   return (
     <header>
-      <h1>Som Timer</h1>
+      <Link to="/">
+        <h1>Som Timer</h1>
+      </Link>
       <nav>
-        <Link to="/settings">
-          <button aria-label="settings">
+        <Link to="/">
+          <button aria-label="timer">
             <img src={metatronsSettings} alt="hexagonal settings icon"/>
           </button>
         </Link>
         <Link to="/stats">
           <button aria-label="stats">
+            <img src={metatronsSettings} alt="hexagonal settings icon"/>
+          </button>
+        </Link>
+        <Link to="/settings">
+          <button aria-label="settings">
+            <img src={metatronsSettings} alt="hexagonal settings icon"/>
+          </button>
+        </Link>
+        <Link to="/about">
+          <button aria-label="about">
             <img src={metatronsSettings} alt="hexagonal settings icon"/>
           </button>
         </Link>
