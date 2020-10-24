47c29fb6d4144fba662283ae884a496b1d8e0a36
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-23 21:16:55 -0600

    Add button for settings and stats #20

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import metatronsSettings from '../../assets/metatronsSettings.png'
 
 const Header = () => {
   return (
@@ -7,14 +8,18 @@ const Header = () => {
       <h1>Som Timer</h1>
       <nav>
         <Link to="/settings">
-          <button name="settings">
-            <img src=/>
+          <button aria-label="settings">
+            <img src={metatronsSettings} alt="hexagonal settings icon"/>
+          </button>
+        </Link>
+        <Link to="/stats">
+          <button aria-label="stats">
+            <img src={metatronsSettings} alt="hexagonal settings icon"/>
           </button>
         </Link>
       </nav>
     </header>
   )
 }
-
 
 export default Header
