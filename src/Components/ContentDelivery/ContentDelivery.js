983134305b0539b0660b43df3294a9297b7d861d
Rachel Williams <rachelwilliams659@gmail.com>
2020-10-23 21:50:12 -0600

    Make ContentDelivery responsive

--- src/Components/ContentDelivery/ContentDelivery.js
+++ src/Components/ContentDelivery/ContentDelivery.js
@@ -7,15 +7,19 @@ const ContentDelivery = () => {
   const [ videoLink ] = useContext(VideoContext)
 
   return (
-    <section className='video'>
-      <ReactPlayer
-        className='video-player'
-        url={videoLink}
-        playing={false}
-        controls={true}
-        width='55em'
-        height='30em'
-      />
+    <section className="video-section">
+      <h2>Enjoy Your Break!</h2>
+      <div class='video-wrapper'>
+        <ReactPlayer
+          className="video-player"
+          alt='Video for pom break'
+          url={videoLink}
+          playing={false}
+          controls={true}
+          width="90%"
+          height="90%"
+        />
+      </div>
       <button className='skip-btn'>Skip video</button>
     </section>
   )
