02ff9151fdde855b8939398d0747a522c8ad4a02
Rachel Williams <rachelwilliams659@gmail.com>
2020-10-24 15:40:01 -0600

    Create conditional rendering for components based on view context

--- src/Components/HomeContainer/HomeContainer.js
+++ src/Components/HomeContainer/HomeContainer.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './HomeContainer.scss'
+import { ViewContext } from '../../Context/ViewContext'
+import { VideoProvider } from '../../Context/VideoContext'
+import ContentDelivery from '../ContentDelivery/ContentDelivery'
 
 const HomeContainer = () => {
+  const [ view ] = useContext(ViewContext)
+
+  return ( 
+    <VideoProvider>
+      { view === 'timer' &&  
+        <h2>Pom Timer View!</h2>
+        // <PomTimer/>
+      }
+      { view === 'content-selection' &&
+      <h2>Content Selection View!</h2>
+        // <ContentSelection/>
+      }
+      { view === 'content-delivery' && 
+        <ContentDelivery />
+      }
+    </VideoProvider>  
+  )
 
 }
 
---
+++ src/Context/ViewContext.js
@@ -0,0 +1,13 @@
+import React, { createContext, useState } from 'react'
+
+export const ViewContext = createContext()
+
+export const ViewProvider = ({ children }) => {
+  const [ view, setView ] = useState('timer')
+
+  return (
+    <ViewContext.Provider value={[view, setView]}>
+      { children}
+    </ViewContext.Provider>
+  )
+}
