cfd0910a65a5f8912de632249f8339e7f4577197
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-23 19:42:49 -0600

    Write first test for header #20

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import Header from './Header'
 import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { MemoryRouter } from 'react-router-dom'
 
+describe('Header', () => {
+  it('should display a title of "Som Timer"', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    )
+
+    const title = getByRole('heading', { name: /som timer/i })
+
+    expect(title).toBeInTheDocument()
+  })
+})
