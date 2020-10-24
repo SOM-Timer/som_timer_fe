db731197b59daec0199245d9afaf2aa01388a443
Jake West <jacobkwest93@gmail.com>
2020-10-24 16:49:11 -0600

    Update timer test in line with team convention #8

--- src/Components/Timer/Timer.test.js
+++ src/Components/Timer/Timer.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { screen, render, fireEvent} from '@testing-library/react'
+import { render } from '@testing-library/react'
 import CountdownTimer from './Timer'
 
 describe('Timer', () => {
     it('Should have the correct content when rendered', () => {
-        render(<CountdownTimer />)
+        const { getByRole, getByText } = render(<CountdownTimer />)
 
-        const startButton = screen.getByRole('button', { name: 'Start' })
-        const resetButton = screen.getByRole('button', { name: 'Reset' })
-        const pauseButton = screen.getByRole('button', { name: 'Pause' })
-        const time = screen.getByText('05:00')
+        const startButton = getByRole('button', { name: 'Start' })
+        const resetButton = getByRole('button', { name: 'Reset' })
+        const pauseButton = getByRole('button', { name: 'Pause' })
+        const time = getByText('05:00')
 
         expect(startButton).toBeInTheDocument();
         expect(resetButton).toBeInTheDocument();
