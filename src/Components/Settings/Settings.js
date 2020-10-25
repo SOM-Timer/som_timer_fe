538df24405db85c76440689e7457981ac0eb69a8
Jake West <jacobkwest93@gmail.com>
2020-10-25 14:33:30 -0600

    Connect SettingsContext to Settings component #26

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import style from './Settings.module.scss'
+import { SettingsContext } from '../../Context/SettingsContext'
 
 const Settings = () => {
-    return(
+    const [ settings, setSettings ] = useContext(SettingsContext)
+    
+    const onChange = (event) => {
+        setSettings({ 
+            ...settings, 
+            [event.target.id]: event.target.value
+        })
+    }
+
+    return (
         <div className={style.settingsContainer}>
             <h1 className={style.settingsHeader}>Settings</h1>
+            <p>Currently, you are set to work for {settings.work} minutes and then take a {settings.break} minute break</p>
             <div className={style.intervalContainer}>
                 <p className={style.intervalLabel}>Select your work interval:</p>
-                <select className={style.intervalDropdown} name='Work' data-testid='workInterval'>
+                <select 
+                    onChange={onChange}
+                    className={style.intervalDropdown} 
+                    name='Work' 
+                    id='work' 
+                    data-testid='workInterval'
+                >
                     <option value={25}>25</option>
                     <option value={30}>30</option>
                     <option value={45}>45</option>
@@ -16,7 +33,13 @@ const Settings = () => {
             </div>
             <div className={style.intervalContainer}>
                 <p className={style.intervalLabel}>Select your break interval:</p>
-                <select className={style.intervalDropdown} name='Break' data-testid='breakInterval'>
+                <select 
+                    onChange={onChange}
+                    className={style.intervalDropdown} 
+                    name='Break' 
+                    id='break' 
+                    data-testid='breakInterval'
+                >
                     <option value={5}>5</option>
                     <option value={7}>7</option>
                     <option value={10}>10</option>
