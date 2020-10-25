44834dfc452169c0a3d33ef8c9af5cf19dec1e92
Jake West <jacobkwest93@gmail.com>
2020-10-25 17:04:40 -0600

    Update SettingsContext interval names #26

--- src/Context/SettingsContext.js
+++ src/Context/SettingsContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react'
 export const SettingsContext = createContext();
 
 export const SettingsProvider = ({ children }) => {
-    const [settings, setSettings] = useState({ work: 25, break: 5 })
+    const [settings, setSettings] = useState({ workInterval: 25, breakInterval: 5 })
 
     return(
         <SettingsContext.Provider value={[ settings, setSettings]}>
