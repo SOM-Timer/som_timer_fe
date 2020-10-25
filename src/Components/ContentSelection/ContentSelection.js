f6d61f1eaf1eb069b82da1e42f3e1cb709ce578c
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-24 23:18:33 -0600

    Rearrange buttons #12

--- src/Components/ContentSelection/ContentSelection.js
+++ src/Components/ContentSelection/ContentSelection.js
@@ -5,26 +5,28 @@ const ContentSelection = () => {
   return (
     <section className={style.ContentSelection}>
       <h2 className={style.prompt}>How would you like to spend your break?</h2>
-      <div>
-        <button 
-          className={style.button} 
-          aria-label="yoga/movement"
+      <div className={style.options}>
+        <div>
+          <button 
+            className={style.button} 
+            aria-label="somatic exercise"
           ></button>
-        <p>Yoga / Movement</p>
-      </div>
-      <div>
-        <button 
-          className={style.button} 
-          aria-label="somatic exercise"
-        ></button>
-        <p>Somatic Exercise</p>
-      </div>
-      <div>
-        <button
-          className={style.button}
-          aria-label="breathwork/meditation"
-        ></button>
-        <p>Breathwork / Meditation</p>
+          <p>Somatic Exercise</p>
+        </div>
+        <div>
+          <button
+            className={style.button}
+            aria-label="breathwork/meditation"
+          ></button>
+          <p>Breathwork / Meditation</p>
+        </div>
+        <div>
+          <button 
+            className={style.button} 
+            aria-label="yoga/movement"
+          ></button>
+          <p>Yoga / Movement</p>
+        </div>
       </div>
     </section>
   );
