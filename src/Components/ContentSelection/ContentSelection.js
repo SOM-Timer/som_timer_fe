a99c7d6a2714904308fe2db4c6eb016da804ce38
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-24 22:48:50 -0600

    Add classnames for styling #12

--- src/Components/ContentSelection/ContentSelection.js
+++ src/Components/ContentSelection/ContentSelection.js
@@ -4,21 +4,30 @@ import style from './ContentSelection.module.scss'
 const ContentSelection = () => {
   return (
     <section className={style.ContentSelection}>
-      <h2>How would you like to spend your break?</h2>
+      <h2 className={style.prompt}>How would you like to spend your break?</h2>
       <div>
-        <button aria-label="yoga/movement"></button>
+        <button 
+          className={style.button} 
+          aria-label="yoga/movement"
+          ></button>
         <p>Yoga / Movement</p>
       </div>
       <div>
-        <button aria-label="somatic exercise"></button>
+        <button 
+          className={style.button} 
+          aria-label="somatic exercise"
+        ></button>
         <p>Somatic Exercise</p>
       </div>
       <div>
-        <button aria-label="breathwork/meditation"></button>
+        <button
+          className={style.button}
+          aria-label="breathwork/meditation"
+        ></button>
         <p>Breathwork / Meditation</p>
       </div>
     </section>
-  )
+  );
 }
 
 export default ContentSelection
