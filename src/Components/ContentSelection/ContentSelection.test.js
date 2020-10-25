d07d9073f755b5f94f41d013e74a13de4f41cb7a
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-24 21:57:37 -0600

    Edit test queries #14

--- src/Components/ContentSelection/ContentSelection.test.js
+++ src/Components/ContentSelection/ContentSelection.test.js
@@ -7,9 +7,9 @@ describe('ContentSelection', () => {
   it('should display the correct contents when rendered', () => {
     const { getByRole } = render(<ContentSelection />)
 
-    const yogaMovementButton = getByRole('button', { name: /yoga \/ movement/i })
+    const yogaMovementButton = getByRole('button', { name: /yoga\/movement/i })
     const somaticExerciseButton = getByRole('button', { name: /somatic exercise/i })
-    const breathworkMeditationButton = getByRole('button', { name: /breathwork \/ meditation/i })
+    const breathworkMeditationButton = getByRole('button', { name: /breathwork\/meditation/i })
 
     expect(yogaMovementButton).toBeInTheDocument()
     expect(somaticExerciseButton).toBeInTheDocument()
