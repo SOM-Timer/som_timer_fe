1582509971210f1c16588a138ed268ccc9173b25
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-24 21:13:18 -0600

    Create imports and shell for first test #14

---
+++ src/Components/ContentSelection/ContentSelection.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContentSelection from './ContentSelection'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('ContentSelection', () => {
+  it('should display the correct contents when rendered', () => {
+    const { getByRole } = render(<ContentSelection />)
+
+    const 
+  })
+})
