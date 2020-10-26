53c09d7a918a90b987aa6d8fa4c55fd681670e45
Rachel Williams <rachelwilliams659@gmail.com>
2020-10-25 19:02:59 -0600

    Add functionality to ContentSelection to change view on button click

--- src/Components/ContentSelection/ContentSelection.js
+++ src/Components/ContentSelection/ContentSelection.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useContext} from 'react'
 import style from './ContentSelection.module.scss'
+import { ViewContext } from '../../Context/ViewContext'
+import { VideoContext } from '../../Context/VideoContext'
 
 const ContentSelection = () => {
+  const [view, setView] = useContext(ViewContext)
+  const [videoLink, setVideoLink] = useContext(VideoContext)
+
+  const fetchVideo = (event) => {
+    const category = event.target.name
+    //get a video from BE with parameters of category from above & breakInterval duration from settings context 
+    //when get video back, use setVideoLink function to set the video url  
+    //may need a loading screen while this is happening? 
+    setView('content-delivery')
+  }
+
   return (
     <section className={style.ContentSelection}>
       <h2 className={style.prompt}>How would you like to spend your break?</h2>
@@ -10,6 +23,8 @@ const ContentSelection = () => {
           <button 
             className={style.button} 
             aria-label="somatic exercise"
+            name="somatic exercise"
+            onClick={fetchVideo}
           ></button>
           <p>Somatic Exercise</p>
         </div>
@@ -17,6 +32,8 @@ const ContentSelection = () => {
           <button
             className={style.button}
             aria-label="breathwork/meditation"
+            name="breathwork/meditation"
+            onClick={fetchVideo}
           ></button>
           <p>Breathwork / Meditation</p>
         </div>
@@ -24,6 +41,8 @@ const ContentSelection = () => {
           <button 
             className={style.button} 
             aria-label="yoga/movement"
+            name="yoga/movement"
+            onClick={fetchVideo}
           ></button>
           <p>Yoga / Movement</p>
         </div>
