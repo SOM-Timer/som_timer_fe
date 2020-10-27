f4b574e47c290be071977cebbe0fd867e31bf973
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-26 20:33:24 -0600

    Wrap component in div for styling #51

--- src/Components/HomeContainer/HomeContainer.js
+++ src/Components/HomeContainer/HomeContainer.js
@@ -1,26 +1,28 @@
 import React, { useContext } from 'react'
-// import style from './HomeContainer.module.scss'
 import { ViewContext } from '../../Context/ViewContext'
 import { VideoProvider } from '../../Context/VideoContext'
 import ContentDelivery from '../ContentDelivery/ContentDelivery'
 import CountdownTimer from '../Timer/Timer'
 import ContentSelection from '../ContentSelection/ContentSelection'
+import style from './HomeContainer.module.scss'
 
 const HomeContainer = () => {
   const [ view ] = useContext(ViewContext)
 
   return ( 
-    <VideoProvider>
-      { view === 'timer' &&  
-        <CountdownTimer/>
-      }
-      { view === 'content-selection' &&
-        <ContentSelection/>
-      }
-      { view === 'content-delivery' && 
-        <ContentDelivery />
-      }
-    </VideoProvider>  
+    <div className={style.HomeContainer}>
+      <VideoProvider>
+        { view === 'timer' &&  
+          <CountdownTimer/>
+        }
+        { view === 'content-selection' &&
+          <ContentSelection/>
+        }
+        { view === 'content-delivery' && 
+          <ContentDelivery />
+        }
+      </VideoProvider>  
+    </div>
   )
 
 }
