07036f169885fe7357a4942d0ee30c21ebbf07a2
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-27 00:05:55 -0600

    Add break in disclaimer and arrow in Contributors

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,11 +9,11 @@ const About = () => {
         <article className={style.appDescription}>
           <p><span className={style.somTimer}>Som Timer</span> is a timer that cares. Inspired by the Pomodoro Technique®, the practice of taking regular breaks at set intervals to fight cognitive boredom and burnout, increase productivity, and boost motivation, Som Timer takes the concept one step further. We at Som Timer believe that what you do on your breaks is just as important as the decision to take those breaks. </p>
           <p>Mindfulness meditation and other somatic exercises have been linked to enhanced productivity, boosted mental health, and stress reduction. To encourage this state of mind and take the guesswork out of your breaks, Som Timer provides mindfulness-related content for you during your break intervals, so you can get back to work feeling refreshed, calm, and ready to tackle the next task.</p>
-          <p className={style.disclaimer}>This timer is not affiliated with, associated with, or endorsed by the Pomodoro Technique® or its creator, Francesco Cirillo. 
+          <p className={style.disclaimer}>This timer is not affiliated with, associated with, or endorsed by the Pomodoro Technique®<br/> or its creator, Francesco Cirillo. 
           </p>
         </article>
       </section>
-      <h2 className={style.broughtToYouBy}>Contributors</h2>
+      <h2 className={style.broughtToYouBy}>Contributors ↓</h2>
       <section className={style.contributors}>
         <section className={style.engineerList}>
           <h3>Front End Engineers: </h3>
