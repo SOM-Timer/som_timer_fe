81cb0d7bed1d48b65c3a1466c51c9c811672387e
Jake West <jacobkwest93@gmail.com>
2020-10-26 21:00:34 -0600

    Fix Timer tab (2 spaces)

--- src/Components/Timer/Timer.js
+++ src/Components/Timer/Timer.js
@@ -13,78 +13,78 @@ import { ViewContext } from '../../Context/ViewContext'
 // let remainingPathColor = COLOR_CODES.info.color
 
 const CountdownTimer = () => {
-    const [ settings ] = useContext(SettingsContext)
-    const [view, setView] = useContext(ViewContext)
-    // const calculateTimeFraction = () => {
-    //     let timeLeft = this.context.value.m * 60 + this.context.value * 1000
-    //     return timeLeft / props.initTime
-    // }
+  const [settings] = useContext(SettingsContext)
+  const [view, setView] = useContext(ViewContext)
+  // const calculateTimeFraction = () => {
+  //     let timeLeft = this.context.value.m * 60 + this.context.value * 1000
+  //     return timeLeft / props.initTime
+  // }
 
-    // const setCircleDashArray =() => {
-    //     const circleDashArray = `${(
-    //         calculateTimeFraction() * 283).toFixed(0)} 283`;
-    //         document.getElementById('base-timer-path-remaining').setAttribute('stroke-dasharray', circleDashArray)
-    // }
+  // const setCircleDashArray =() => {
+  //     const circleDashArray = `${(
+  //         calculateTimeFraction() * 283).toFixed(0)} 283`;
+  //         document.getElementById('base-timer-path-remaining').setAttribute('stroke-dasharray', circleDashArray)
+  // }
 
-    const timerDone = () => {
-        setView('content-selection')
-    }
+  const timerDone = () => {
+    setView('content-selection')
+  }
 
-    return (
-        <div className={style.countdownTimer}>
-            <Timer 
-                direction='backward'
-                initialTime={settings.workInterval * 60000}
-                lastUnit='m'
-                startImmediately={false}
-                timeToUpdate={10}
-                checkpoints={[
-                    {
-                        time: 0,
-                        callback: () => timerDone(),
-                    },
-                ]}
-            >
-                {({ start, pause, reset }) => (
-                    <>
-                        <div className={style.baseTimer}>
-                            <svg className={style.baseTimerSvg} viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
-                                <g className={style.baseTimerCircle}>
-                                    <circle className={style.baseTimerPathElapsed} cx='50' cy='50' r='45' />
-                                    <path
-                                        id='base-timer-path-remaining'
-                                        strokeDasharray='283'
-                                        className={style.baseTimerPathRemaining}
-                                        d="
-                                            M 50, 50
-                                            m -45, 0
-                                            a 45,45 0 1,0 90,0
-                                            a 45,45 0 1,0 -90,0
-                                        "
-                                    />
-                                </g>
-                            </svg>
-                            <p className={style.baseTimerLabel}>
-                                {<Timer.Minutes />}:{<Timer.Seconds 
-                                    formatValue={(value) => `${(value < 10 ? `0${value}` : value)}`}
-                                />}
-                            </p>
-                        </div>
-                        <div className={style.timerControls}>
-                            <button className={style.timerControlButton} onClick={() => {
-                                start();
-                                // setCircleDashArray();
-                            }}>
-                                Start
-                            </button>
-                            <button className={style.timerControlButton} onClick={pause}>Pause</button>
-                            <button className={style.timerControlButton} onClick={reset}>Reset</button>
-                        </div>
-                    </>
-                )}
-            </Timer>
-        </div>
-    )
+  return (
+    <div className={style.countdownTimer}>
+      <Timer
+        direction='backward'
+        initialTime={settings.workInterval * 60000}
+        lastUnit='m'
+        startImmediately={false}
+        timeToUpdate={10}
+        checkpoints={[
+          {
+            time: 0,
+            callback: () => timerDone(),
+          },
+        ]}
+      >
+        {({ start, pause, reset }) => (
+          <>
+            <div className={style.baseTimer}>
+              <svg className={style.baseTimerSvg} viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+                <g className={style.baseTimerCircle}>
+                  <circle className={style.baseTimerPathElapsed} cx='50' cy='50' r='45' />
+                  <path
+                    id='base-timer-path-remaining'
+                    strokeDasharray='283'
+                    className={style.baseTimerPathRemaining}
+                    d="
+                      M 50, 50
+                      m -45, 0
+                      a 45,45 0 1,0 90,0
+                      a 45,45 0 1,0 -90,0
+                    "
+                  />
+                </g>
+              </svg>
+              <p className={style.baseTimerLabel}>
+                {<Timer.Minutes />}:{<Timer.Seconds
+                  formatValue={(value) => `${(value < 10 ? `0${value}` : value)}`}
+                />}
+              </p>
+            </div>
+            <div className={style.timerControls}>
+              <button className={style.timerControlButton} onClick={() => {
+                start();
+                // setCircleDashArray();
+              }}>
+                Start
+              </button>
+              <button className={style.timerControlButton} onClick={pause}>Pause</button>
+              <button className={style.timerControlButton} onClick={reset}>Reset</button>
+            </div>
+          </>
+        )}
+      </Timer>
+    </div>
+  )
 }
 
 export default CountdownTimer;
