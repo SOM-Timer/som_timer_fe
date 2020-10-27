e0213e6769508a9239c20aa7dc2a3ce469003078
Jake West <jacobkwest93@gmail.com>
2020-10-27 13:36:39 -0600

    Refactor circle dash array #41

--- src/Components/Timer/Timer.js
+++ src/Components/Timer/Timer.js
@@ -4,27 +4,9 @@ import style from './Timer.module.scss';
 import { SettingsContext } from '../../Context/SettingsContext'
 import { ViewContext } from '../../Context/ViewContext'
 
-// const COLOR_CODES = {
-//     info: {
-//         color: '#F4D2D0',
-//     }
-// }
-
-// let remainingPathColor = COLOR_CODES.info.color
-
 const CountdownTimer = () => {
   const [settings] = useContext(SettingsContext)
   const [view, setView] = useContext(ViewContext)
-  // const calculateTimeFraction = () => {
-  //     let timeLeft = this.context.value.m * 60 + this.context.value * 1000
-  //     return timeLeft / props.initTime
-  // }
-
-  // const setCircleDashArray =() => {
-  //     const circleDashArray = `${(
-  //         calculateTimeFraction() * 283).toFixed(0)} 283`;
-  //         document.getElementById('base-timer-path-remaining').setAttribute('stroke-dasharray', circleDashArray)
-  // }
 
   const timerDone = () => {
     setView('content-selection')
@@ -45,7 +27,7 @@ const CountdownTimer = () => {
           },
         ]}
       >
-        {({ start, pause, reset }) => (
+        {({ start, pause, reset, getTime }) => (
           <>
             <div className={style.baseTimer}>
               <svg className={style.baseTimerSvg} viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
@@ -53,7 +35,7 @@ const CountdownTimer = () => {
                   <circle className={style.baseTimerPathElapsed} cx='50' cy='50' r='45' />
                   <path
                     id='base-timer-path-remaining'
-                    strokeDasharray='283'
+                    strokeDasharray={`${getTime() / (settings.workInterval * 60000) * 283} 283`}
                     className={style.baseTimerPathRemaining}
                     d="
                       M 50, 50
