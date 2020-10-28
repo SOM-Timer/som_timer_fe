6f927cea38e698b48ea1f73c0f97b37810048897
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-27 20:40:58 -0600

    Style warning symbols and turn li into p

--- src/Components/PageNotFound/PageNotFound.js
+++ src/Components/PageNotFound/PageNotFound.js
@@ -5,19 +5,32 @@ import burnout from '../../assets/burnout3.jpg'
 const PageNotFound = () => {
   return (
     <section>
-      <h2 className={style.errorCode}>⌇ 404 ⌇</h2>
-      <h3 className={style.oopsMessage}>Oops, we can't seem to find the page you're looking for!</h3>
-      <img className={style.burnout} src={burnout} alt='distressed internet user'/>
+      <h2 className={style.errorCode}>
+        <span className={style.warning}>⚠︎</span> 404{" "}
+        <span className={style.warning}>⚠︎</span>
+      </h2>
+      <h3 className={style.oopsMessage}>
+        Oops, we can't seem to find the page you're looking for!
+      </h3>
+      <img
+        className={style.burnout}
+        src={burnout}
+        alt="distressed internet user"
+      />
       <section className={style.notFoundContent}>
         <p>Here are some helpful links to get you back on track:</p>
-        <ul>
-          <li><a href='/'>Home / Timer</a></li>
-          <li><a href='/settings'>Settings</a></li>
-          <li><a href='/about'>About</a></li>
-        </ul>
+        <a href="/">
+          <p>Home / Timer</p>
+        </a>
+        <a href="/settings">
+          <p>Settings</p>
+        </a>
+        <a href="/about">
+          <p>About</p>
+        </a>
       </section>
     </section>
-  )
+  );
 }
 
 export default PageNotFound
