6f912d745b8c4349fed68d2529d4148129dc8327
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-27 20:26:27 -0600

    Add error code heading

--- src/Components/PageNotFound/PageNotFound.js
+++ src/Components/PageNotFound/PageNotFound.js
@@ -5,7 +5,8 @@ import burnout from '../../assets/burnout3.jpg'
 const PageNotFound = () => {
   return (
     <section>
-      <h2>Oops, we can't seem to find the page you're looking for!</h2>
+      <h2 className={style.errorCode}>⌇ 404 ⌇</h2>
+      <h3 className={style.oopsMessage}>Oops, we can't seem to find the page you're looking for!</h3>
       <img className={style.burnout} src={burnout} alt='distressed internet user'/>
       <section className={style.notFoundContent}>
         <p>Here are some helpful links to get you back on track:</p>
