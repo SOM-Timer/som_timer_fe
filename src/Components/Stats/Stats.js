2d328b387ff7383bd877a7016244c781f4d9dd06
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-27 21:00:29 -0600

    Add classname to heading prompt

--- src/Components/Stats/Stats.js
+++ src/Components/Stats/Stats.js
@@ -4,9 +4,9 @@ import style from './Stats.module.scss'
 const Stats = () => {
   return (
     <article>
-      <h2>Stats Page Coming Soon!</h2>
+      <h2 className={style.prompt}>Stats Page Coming Soon!</h2>
       <section className={style.statsDesc}>
-        <p>Check back soon for personal stats so you can see the positive impact Som Timer is having on your productivity and mental health. Stats will include data such as a log of pom cycles taken, mood and engagement tracking, and what types of content work best for you!</p>
+        <p><strong>Check back soon</strong> for personal stats so you can see the positive impact Som Timer is having on your productivity and mental health. Stats will include data such as a log of pom cycles taken, mood and engagement tracking, and what types of content work best for you!</p>
       </section>
     </article>
   )
