ee8382e12622a4e1903a2f1102ff1a31fe440ec2
Jake West <jacobkwest93@gmail.com>
2020-10-29 09:00:00 -0600

    Add getRandomContent network request #46

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+const rootUrl = 'https://som-timer-be.herokuapp.com'
 
 export const getSettings = () => {
   return axios.get('https://som-timer-be.herokuapp.com/api/timers/1')
@@ -8,5 +9,13 @@ export const updateSettings = (settings) => {
   return axios.put('https://som-timer-be.herokuapp.com/api/timers/1', {
     work_interval: settings.workInterval,
     rest_interval: settings.breakInterval
+  })
+}
+
+export const getRandomContent = (duration, category) => {
+  return axios.get(`${rootUrl}/api/rand_exercise`, {
+    //I think this body just be { duration, category }! (copied documentation for now)
+    "duration": duration,
+    "category": category,
   })
 }
