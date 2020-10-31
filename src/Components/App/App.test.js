ee37a277b15ec8959dcf6beb5146cd8c6a4d7172
Rachel Williams <rachelwilliams659@gmail.com>
2020-10-31 17:18:14 -0600

    Add App test for a user updating work interval and seeing that in timer

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { act } from 'react-dom/test-utils'
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 import '@testing-library/jest-dom'
@@ -9,8 +10,9 @@ import { MemoryRouter } from 'react-router-dom'
 import { getSettings, updateSettings } from '../../apiCalls'
 jest.mock('../../apiCalls.js')
 
+
 describe('App', () => {
-  it('should bring users to the timer view on load and allow them to visit the Stats, About, and Settings pages', () => {
+  it('should bring users to the timer view on load and allow them to visit the Stats, About, and Settings pages', async () => {
 
     getSettings.mockResolvedValue({
       data: {
@@ -20,7 +22,7 @@ describe('App', () => {
       }
     })
 
-    const { getByRole, getByAltText } = render(
+    const { getByRole, getByAltText, getByText } = render(
       <MemoryRouter>
         <SettingsProvider>
           <ViewProvider>
@@ -32,9 +34,11 @@ describe('App', () => {
       </MemoryRouter>
     )
 
+    const timerLength = await waitFor(() => getByText(/30:00/i))
     const appHeading = getByRole('heading', { name: /som timer/i })
     const timerHeading = getByRole('heading', { name: /click ▶ to begin focusing/i })
 
+    expect(timerLength).toBeInTheDocument()
     expect(appHeading).toBeInTheDocument()
     expect(timerHeading).toBeInTheDocument()
 
@@ -65,5 +69,44 @@ describe('App', () => {
     const timerHeading2 = getByRole('heading', { name: /click ▶ to begin focusing/i })
 
     expect(timerHeading2).toBeInTheDocument()
+  })
+
+  it('should allow a user to update work interval in settings and see that reflected in the timer view', async () => {
+
+    getSettings.mockResolvedValueOnce({
+      data: {
+        id: 1,
+        rest_interval: '5',
+        work_interval: '30'
+      }
+    })
+
+    updateSettings.mockResolvedValue('Success!')
+
+    const { getByRole, getByAltText, getByText } = render(
+      <MemoryRouter>
+        <SettingsProvider>
+          <ViewProvider>
+            <VideoProvider>
+              <App />
+            </VideoProvider>
+          </ViewProvider>
+        </SettingsProvider>
+      </MemoryRouter>
+    )
+
+    const settingsIcon = getByAltText('settings icon')
+    fireEvent.click(settingsIcon)
+
+    const workIntervalInput = getByRole('spinbutton')
+    
+    fireEvent.blur(workIntervalInput, { target: { value: '45'}})
+
+    const homeIcon = getByAltText('timer icon')
+    fireEvent.click(homeIcon)
+
+    const timerLength = await waitFor(() => getByText(/45:00/i))
+
+    expect(timerLength).toBeInTheDocument()
   })
 })
