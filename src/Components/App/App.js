c7fd9d9af1e8b5961a647e53457ae86bb7fc8504
Rachel Williams <rachelwilliams659@gmail.com>
2020-11-01 11:10:45 -0700

    Remove commented out code from App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,4 @@
 import React from 'react'
-// import { ViewProvider } from '../../Context/ViewContext'
-// import { SettingsProvider } from '../../Context/SettingsContext'
 import { Switch, Route } from 'react-router-dom'
 import HomeContainer from '../HomeContainer/HomeContainer'
 import Header from '../Header/Header'
@@ -11,28 +9,24 @@ import PageNotFound from '../PageNotFound/PageNotFound'
 import './App.scss'
 
 function App() {
-  if (!('Notification' in window)) {
-    console.log('This browser does not support notifications!')
-    return;
-  }
+  // if (!('Notification' in window)) {
+  //   console.log('This browser does not support notifications!')
+  //   return;
+  // }
 
-  Notification.requestPermission(status => {
-    console.log('Notification permission status:', status)
-  })
+  // Notification.requestPermission(status => {
+  //   console.log('Notification permission status:', status)
+  // })
   
   return (
     <section className="App">
       <Header />
       <Switch>
-        {/* <SettingsProvider> */}
-          {/* <ViewProvider> */}
             <Route exact path="/" render={() => <HomeContainer />} />
-          {/* </ViewProvider> */}
           <Route exact path="/settings" render={() => <Settings />} />
           <Route exact path="/about" render={() => <About />} />
           <Route exact path="/stats" render={() => <Stats />} />
           <Route render={() => <PageNotFound />} />
-        {/* </SettingsProvider> */}
       </Switch>
     </section>
   );
