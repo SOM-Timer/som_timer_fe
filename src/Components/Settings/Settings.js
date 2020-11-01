5d1764f19ad75e4cec856063ed6578764be472cd
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-31 22:05:20 -0600

    Add dropdown menu to select a sound #63

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -2,6 +2,13 @@ import React, { useContext, useEffect } from 'react'
 import style from './Settings.module.scss'
 import { SettingsContext } from '../../Context/SettingsContext'
 import { updateSettings } from '../../apiCalls'
+// import gong from '../../assets/audio/gong.mp3'
+// import goodOldSynths from '../../assets/audio/goodOldSynths.mp3'
+// import pianoDreams from '../../assets/audio/pianoDreams.mp3'
+// import chordCliff from '../../assets/audio/chordCliff.mp3'
+// import levelUp from '../../assets/audio/levelUp.mp3'
+// import birdChord from '../../assets/audio/birdChord.mp3'
+
 
 const Settings = () => {
   const [settings, setSettings] = useContext(SettingsContext)
@@ -13,7 +20,7 @@ const Settings = () => {
   const onChange = (event) => {
     setSettings({
       ...settings,
-      [event.target.id]: +event.target.value
+      [event.target.id]: event.target.value
     })
   }
 
@@ -48,6 +55,24 @@ const Settings = () => {
           <option value={10}>10</option>
         </select>
         <p>minutes</p>
+      </div>
+      <div className={style.intervalContainer}>
+        <p className={style.intervalLabel}>Select your alert sound:</p>
+        <select
+          onChange={onChange}
+          className={style.breakIntervalDropdown}
+          name='Sound'
+          id='sound'
+          data-testid='sound'
+          value={settings.sound}
+        >
+          <option value={'chordCliff'}>Reverb Splash</option>
+          <option value={'gong'}>Balinese Gong</option>
+          <option value={'goodOldSynths'}>Good Ol' Synthesizers</option>
+          <option value={'pianoDreams'}>Piano Dreams</option>
+          <option value={'levelUp'}>Level Up</option>
+          <option value={'birdChord'}>Bird Chord</option>
+        </select>
       </div>
       <p className={style.saveAutomatically}>✨ Settings will save automagically ✨</p>
     </div>
