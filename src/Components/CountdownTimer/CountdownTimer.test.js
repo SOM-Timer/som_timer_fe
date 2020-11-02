7f55262aec49f4f2693a7f309865a4a1d6840791
Rachel Williams <rachelwilliams659@gmail.com>
2020-11-01 17:19:04 -0700

    Add timer test for calling correct function when timer runs out

--- src/Components/CountdownTimer/CountdownTimer.test.js
+++ src/Components/CountdownTimer/CountdownTimer.test.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react'
+import playAlertSound from '../../helpers/audioHelper'
+import { fireEvent, render, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import CountdownTimer from './CountdownTimer'
 import { SettingsProvider } from '../../Context/SettingsContext'
 import { ViewProvider } from '../../Context/ViewContext'
+import { getSettings } from '../../apiCalls'
+jest.mock('../../apiCalls.js')
 
 describe('CountdownTimer', () => {
-  it('Should have the correct content when rendered', () => {
+  it('Should have the correct content when rendered', async () => {
+
+    getSettings.mockResolvedValue({
+      data: {
+        id: 1,
+        rest_interval: '5:00',
+        work_interval: '0.1',
+        sound: 'chordCliff'
+      }
+    })
+
     const { getByRole } = render(
       <SettingsProvider>
         <ViewProvider>
@@ -14,7 +28,7 @@ describe('CountdownTimer', () => {
       </SettingsProvider>
     )
 
-    const focusHeading = getByRole('heading', { name: 'Click ▶ to Begin Focusing'})
+    const focusHeading = await waitFor(() => getByRole('heading', { name: 'Click ▶ to Begin Focusing'}))
     const startButton = getByRole('button', { name: /start/i })
     const resetButton = getByRole('button', { name: /reset/i })
     const skipButton = getByRole('button', { name: /skip/i })
@@ -23,5 +37,35 @@ describe('CountdownTimer', () => {
     expect(startButton).toBeInTheDocument();
     expect(resetButton).toBeInTheDocument();
     expect(skipButton).toBeInTheDocument();
+  })
+
+  it('should fire the timerDone function when the timer runs out', async () => {
+
+    getSettings.mockResolvedValue({
+      data: {
+        id: 1,
+        rest_interval: '5:00',
+        work_interval: '0.1',
+        sound: 'chordCliff'
+      }
+    })
+
+    const { getByRole } = render(
+      <SettingsProvider>
+        <ViewProvider>
+          <CountdownTimer />
+        </ViewProvider>
+      </SettingsProvider>
+    )
+
+    const startButton = await waitFor(() => getByRole('button', { name: /start/i }))
+    
+    fireEvent.click(startButton)
+
+    setTimeout(() => {
+      expect(playAlertSound).toHaveBeenCalledTimes(1)
+      expect(playAlertSound).toHaveBeenCalledWith('chordCliff')
+    }, 6000)
+
   })
 })
