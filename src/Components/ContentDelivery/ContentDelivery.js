7375ec36b519677c9e722fc2ae92eada64caca65
Jake West <jacobkwest93@gmail.com>
2020-11-02 16:13:26 -0700

    Add session context functionality to content delivery #73
    Co-authored-by: Rachel Williams <62770765+rwilliams659@users.noreply.github.com>
    Co-authored-by: Aaron Burris-Deboskey <61372592+Abdeboskey@users.noreply.github.com>

--- src/Components/ContentDelivery/ContentDelivery.js
+++ src/Components/ContentDelivery/ContentDelivery.js
@@ -3,14 +3,26 @@ import style from './ContentDelivery.module.scss'
 import ReactPlayer from 'react-player'
 import { VideoContext } from '../../Context/VideoContext'
 import { ViewContext } from '../../Context/ViewContext'
+import { SettingsContext } from '../../Context/SettingsContext'
+import { SessionContext } from '../../Context/SessionContext'
 
 const ContentDelivery = () => {
   const [ done, setDone ] = useState(false)
   const [ videoLink ] = useContext(VideoContext)
   const [ view, setView ] = useContext(ViewContext)
+  const [ settings ] = useContext(SettingsContext)
+  const [ session, setSession ] = useContext(SessionContext)
+
+  const recordBreakInterval = () => {
+    setSession({
+      ...session,
+      restInterval: settings.breakInterval
+    })
+  }
 
   const handleEnded = () => {
     setDone(true)
+    recordBreakInterval()
     setTimeout(() => setView('mood-rating-2'), 2000)
   }
 
@@ -30,7 +42,15 @@ const ContentDelivery = () => {
           />
         </div>
         { !done &&
-          <button className={style.skipBtn} onClick={() => setView('mood-rating-2')}>Skip video</button>     
+          <button 
+            className={style.skipBtn} 
+            onClick={() => {
+              setView('mood-rating-2')
+              recordBreakInterval()
+            }}
+          >
+            Skip video
+          </button>     
         }
       </section>
       { done && 
