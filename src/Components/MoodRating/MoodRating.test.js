04c0fce76220b614ab787071321afacd6fa3c5d0
Jake West <jacobkwest93@gmail.com>
2020-11-03 09:08:31 -0700

    Add errorMessage test coverage #73

--- src/Components/MoodRating/MoodRating.test.js
+++ src/Components/MoodRating/MoodRating.test.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import MoodRating from './MoodRating'
+import { ViewProvider } from '../../Context/ViewContext'
+import { SessionProvider } from '../../Context/SessionContext'
+import { postSession } from '../../apiCalls'
+jest.mock('../../apiCalls')
 
 describe('MoodRating', () => {
   it('Should have the correct content when rendered', () => {
-    const { getByRole } = render(
-      <MoodRating />
+    const { getByRole, debug } = render(
+        <ViewProvider>
+          <SessionProvider>
+            <MoodRating />
+          </SessionProvider>
+        </ViewProvider>
     )
 
-    const moodHeading = getByRole('heading', /Please select a face/i)
+    const moodHeading = getByRole('heading', /please select a face/i)
     const moodRating1 = getByRole('button', { name: /1/ })
     const moodRating2 = getByRole('button', { name: /2/ })
     const moodRating3 = getByRole('button', { name: /3/ })
@@ -23,5 +31,42 @@ describe('MoodRating', () => {
     expect(moodRating4).toBeInTheDocument()
     expect(moodRating5).toBeInTheDocument()
     expect(submitButton).toBeInTheDocument()
+  })
+
+  it('Should provide an error message if the form is submitted without a selected value', () => {
+    const { getByRole, getByText } = render(
+      <ViewProvider>
+        <SessionProvider>
+          <MoodRating />
+        </SessionProvider>
+      </ViewProvider>
+    )
+
+    const submitButton = getByRole('button', { name: /Submit/ })
+    fireEvent.click(submitButton)
+    const errorMessage = getByText(/please select an option above to continue/i)
+
+    expect(errorMessage).toBeInTheDocument()
+  })
+
+  it('Should clear the error message once a user selects an option', () => {
+    const { getByRole, getByText } = render(
+      <ViewProvider>
+        <SessionProvider>
+          <MoodRating />
+        </SessionProvider>
+      </ViewProvider>
+    )
+
+    const submitButton = getByRole('button', { name: /Submit/ })
+    fireEvent.click(submitButton)
+    const errorMessage = getByText(/please select an option above to continue/i)
+    
+    expect(errorMessage).toBeInTheDocument()
+
+    const moodIcon4 = getByRole('button', { name: /4/})
+    fireEvent.click(moodIcon4)
+    
+    expect(errorMessage).not.toBeInTheDocument()
   })
 })
