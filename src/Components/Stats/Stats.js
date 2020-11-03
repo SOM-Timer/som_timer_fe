f5142dbffe8b634c0b9569b86a35e8f21821f85e
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-11-03 14:14:58 -0700

    Add span alt and aria label for emoji accessibility #81

--- src/Components/Stats/Stats.js
+++ src/Components/Stats/Stats.js
@@ -4,12 +4,26 @@ import style from './Stats.module.scss'
 const Stats = () => {
   return (
     <article>
-      <h2 className={style.prompt}>⏳ Stats Page Coming Soon! ⏳</h2>
+      <h2 className={style.prompt}>
+        <span role="img" alt="hourglass emoji" aria-label="hourglass emoji">
+          ⏳
+        </span>{" "}
+        Stats Page Coming Soon!{" "}
+        <span role="img" alt="hourglass emoji" aria-label="hourglass emoji">
+          ⏳
+        </span>
+      </h2>
       <section className={style.statsDesc}>
-        <p><strong className={style.strong}>Check back soon</strong> for personal stats so you can see the positive impact Som Timer is having on your productivity and mental health. Stats will include data such as a log of pom cycles taken, mood and engagement tracking, and what types of content work best for you!</p>
+        <p>
+          <strong className={style.strong}>Check back soon</strong> for personal
+          stats so you can see the positive impact Som Timer is having on your
+          productivity and mental health. Stats will include data such as a log
+          of pom cycles taken, mood and engagement tracking, and what types of
+          content work best for you!
+        </p>
       </section>
     </article>
-  )
+  );
 }
 
 export default Stats
