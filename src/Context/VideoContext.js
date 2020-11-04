c88ccf8df1b06588732a2310f3d34c4f19f27bc0
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-11-03 20:54:53 -0700

    Set initial video context to null to fix console errors #81

--- src/Context/VideoContext.js
+++ src/Context/VideoContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react'
 export const VideoContext = createContext()
 
 export const VideoProvider = ({ children }) => {
-  const [videoLink, setVideoLink] = useState('https://www.youtube.com/watch?v=4C-gxOE0j7s')
+  const [videoLink, setVideoLink] = useState(null)
 
   return (
     <VideoContext.Provider value={ [videoLink, setVideoLink ]}>
