5103aac4b2d3dcd48655a2f9edba0159e66a3606
Jake West <jacobkwest93@gmail.com>
2020-11-04 09:29:07 -0700

    Remove Coming Soon article #83

--- src/Components/Stats/Stats.js
+++ src/Components/Stats/Stats.js
@@ -117,28 +117,6 @@ const Stats = ({ toggleTimerView }) => {
           </h3>
         </section>
       </div>
-      <article>
-        <h2 className={style.prompt}>
-          <span role="img" alt="hourglass emoji" aria-label="hourglass emoji">
-            ⏳
-        </span>{" "}
-        Stats Page Coming Soon!{" "}
-          <span role="img" alt="hourglass emoji" aria-label="hourglass emoji">
-            ⏳
-        </span>
-        </h2>
-        <section className={style.statsDesc}>
-          <div className={style.line}></div>
-          <p>
-            <strong className={style.strong}>Check back soon</strong> for personal
-          stats, so you can see the positive impact Som Timer is having on your
-          productivity and mental health. Stats will include data such as a log
-          of pom cycles taken, mood and engagement tracking, and what types of
-          content work best for you!
-        </p>
-          <div className={style.line}></div>
-        </section>
-      </article>
     </>
   )
 }
