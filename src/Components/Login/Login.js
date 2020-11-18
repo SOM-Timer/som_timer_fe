6e46df9787cf5ef35bd7565c0acd2a3bdd60ed81
Jake West <jacobkwest93@gmail.com>
2020-11-18 12:36:15 -0700

    Add walkthrough implementation code #94

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -12,6 +12,52 @@ const Login = () => {
       <p className={style.appDescription}>Som Timer is a timer that cares. This application serves curated wellness content on break intervals. Build in rest so you can focus best!</p>
       {/* This login button comes from these docs https://developers.google.com/identity/sign-in/web/sign-in; We should explore these further for methods we'll need for Login */}
       <div className='g-signin2' data-onsuccess='onSignIn' aria-label='Google Login Button'></div>
+      <section className={style.somTimerWalkthrough}>
+        <article className={style.walkthroughArticle}>
+          <img
+            className={style.icon}
+            src={timerIcon}
+            alt="timer icon"
+          />
+          <div className={style.walkthroughText}>
+            <h3 className={style.walkthroughHeader}>Home Page</h3>
+            <p className={style.walkthroughDescription}>On the home page, you can view your current focus or break interval in progress. Here, you'll find the countdown timer and built in media player that provides your wellness content.</p>
+          </div>
+        </article>
+        <article className={style.walkthroughArticle}>
+          <img
+            className={style.icon}
+            src={settingsIcon}
+            alt="settings icon"
+          />
+          <div className={style.walkthroughText}>
+            <h3 className={style.walkthroughHeader}>Settings Page</h3>
+            <p className={style.walkthroughDescription}>On the settings page, you can update your focus and break intervals to fit your needs. You can also adjust other preferences, like the sound of your notifications.</p>
+          </div>
+        </article>
+        <article className={style.walkthroughArticle}>
+          <img
+            className={style.icon}
+            src={statsIcon}
+            alt="stats icon"
+          />
+          <div className={style.walkthroughText}>
+            <h3 className={style.walkthroughHeader}>Stats Page</h3>
+            <p className={style.walkthroughDescription}>On the stats page, you can get up to date information about your Som Timer usage. More info coming soon!</p>
+          </div>
+        </article>
+        <article className={style.walkthroughArticle}>
+          <img
+            className={style.icon}
+            src={aboutIcon}
+            alt="about icon"
+          />
+          <div className={style.walkthroughText}>
+            <h3 className={style.walkthroughHeader}>About Page</h3>
+            <p className={style.walkthroughDescription}>On the about page, you can read more about our applciation. You'll also find helpful resources and information about the apps contributors.</p>
+          </div>
+        </article>
+      </section>
     </section>
   )
 }
