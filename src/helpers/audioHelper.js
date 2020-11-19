1b13cde752fe39cb55f24fd5f491163a65acb763
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-11-18 19:10:17 -0700

    Clean up formatting #77

--- src/helpers/audioHelper.js
+++ src/helpers/audioHelper.js
@@ -1,11 +1,18 @@
-import balineseGong from "../assets/audio/balineseGong.mp3";
-import goodOldSynths from "../assets/audio/goodOldSynths.mp3";
-import pianoDreams from "../assets/audio/pianoDreams.mp3";
-import reverbSplash from "../assets/audio/reverbSplash.mp3";
-import levelUp from "../assets/audio/levelUp.mp3";
-import birdChord from "../assets/audio/birdChord.mp3";
+import balineseGong from '../assets/audio/balineseGong.mp3'
+import goodOldSynths from '../assets/audio/goodOldSynths.mp3'
+import pianoDreams from '../assets/audio/pianoDreams.mp3'
+import reverbSplash from '../assets/audio/reverbSplash.mp3'
+import levelUp from '../assets/audio/levelUp.mp3'
+import birdChord from '../assets/audio/birdChord.mp3'
 
-const notificationSounds = [balineseGong, goodOldSynths, pianoDreams, reverbSplash, levelUp, birdChord]
+const notificationSounds = [
+  balineseGong, 
+  goodOldSynths, 
+  pianoDreams, 
+  reverbSplash, 
+  levelUp, 
+  birdChord
+]
 
 const getRandomNotification = (sounds) => {
   const randomIndex =  Math.floor(Math.random() * sounds.length)
@@ -13,31 +20,31 @@ const getRandomNotification = (sounds) => {
 }
 
 export const playAlertSound = (sound) => {
-  let audioNotification = "";
+  let audioNotification = ''
   switch (sound) {
-    case "reverbSplash":
-      audioNotification = new Audio(reverbSplash);
-      break;
-    case "balineseGong":
-      audioNotification = new Audio(balineseGong);
-      break;
-    case "goodOldSynths":
-      audioNotification = new Audio(goodOldSynths);
-      break;
-    case "pianoDreams":
-      audioNotification = new Audio(pianoDreams);
-      break;
-    case "levelUp":
-      audioNotification = new Audio(levelUp);
-      break;
-    case "birdChord":
-      audioNotification = new Audio(birdChord);
-      break;
-    case "random":
+    case 'reverbSplash':
+      audioNotification = new Audio(reverbSplash)
+      break
+    case 'balineseGong':
+      audioNotification = new Audio(balineseGong)
+      break
+    case 'goodOldSynths':
+      audioNotification = new Audio(goodOldSynths)
+      break
+    case 'pianoDreams':
+      audioNotification = new Audio(pianoDreams)
+      break
+    case 'levelUp':
+      audioNotification = new Audio(levelUp)
+      break
+    case 'birdChord':
+      audioNotification = new Audio(birdChord)
+      break
+    case 'random':
       audioNotification = new Audio(getRandomNotification(notificationSounds))
-      break;
+      break
     default:
-      audioNotification = new Audio(goodOldSynths);
+      audioNotification = new Audio(goodOldSynths)
   }
-  audioNotification.play();
-};
+  audioNotification.play()
+}
