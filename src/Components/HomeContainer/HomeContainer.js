19733025d1354a61143381e6c7419227ee11f553
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-11-23 12:46:54 -0700

    Add usercontext and conditional rendering for user #106

--- src/Components/HomeContainer/HomeContainer.js
+++ src/Components/HomeContainer/HomeContainer.js
@@ -1,28 +1,35 @@
 import React, { useContext } from 'react'
 import { ViewContext } from '../../Context/ViewContext'
+import { UserContext } from '../../Context/UserContext'
 import { VideoProvider } from '../../Context/VideoContext'
 import ContentDelivery from '../ContentDelivery/ContentDelivery'
 import CountdownTimer from '../CountdownTimer/CountdownTimer'
 import ContentSelection from '../ContentSelection/ContentSelection'
 import MoodRating from '../MoodRating/MoodRating'
+import Login from '../Login/Login'
 import style from './HomeContainer.module.scss'
 
 const HomeContainer = () => {
   const [ view ] = useContext(ViewContext)
+  const [ user ] = useContext(UserContext)
 
   return (
     <div className={style.HomeContainer} id="HomeContainer">
       <VideoProvider>
-        {view === "timer" && <CountdownTimer />}
-        {view === "content-selection" && <ContentSelection />}
-        {view === "content-delivery" && <ContentDelivery />}
-        {(view === "mood-rating-1" || view === "mood-rating-2") && (
-          <MoodRating />
+        {!user && <Login />}
+        {user && (
+          <>
+            {view === "timer" && <CountdownTimer />}
+            {view === "content-selection" && <ContentSelection />}
+            {view === "content-delivery" && <ContentDelivery />}
+            {(view === "mood-rating-1" || view === "mood-rating-2") && (
+              <MoodRating />
+            )}
+          </>
         )}
       </VideoProvider>
     </div>
   );
-
 }
 
 export default HomeContainer
