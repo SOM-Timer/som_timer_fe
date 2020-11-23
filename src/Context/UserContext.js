590a3acb123e99e75cc45cd50b70e09544e8c277
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-11-23 15:57:43 -0700

    Add useeffect hook #106

--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -4,6 +4,18 @@ export const UserContext = createContext()
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState({ userName: '', token: '', userId: null })
+
+  useEffect(() => {
+    const credentials = JSON.parse(localStorage.getItem('somTimerUser'))
+    if (credentials) {
+      setUser({
+        ...user,
+        userName: credentials.userName,
+        token: credentials.token,
+        userId: credentials.userId
+      })
+    }
+  }, [])
   
   return (
     <UserContext.Provider value={[ user, setUser ]} >
