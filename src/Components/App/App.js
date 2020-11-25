8dc2ca4fc1ff01e0d56d89cbce0ba525167bf007
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-11-24 22:11:51 -0700

    Move login check from homecontainer to app #106

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HomeContainer from '../HomeContainer/HomeContainer'
 import Header from '../Header/Header'
 import Settings from '../Settings/Settings'
 import About from '../About/About'
 import Stats from '../Stats/Stats'
+import Login from '../Login/Login'
+import { UserContext } from '../../Context/UserContext'
 import PageNotFound from '../PageNotFound/PageNotFound'
 import { enableNotifications } from '../../helpers/notificationHelpers'
 import { ToastContainer } from 'react-toastify'
+
 import './App.scss'
 
 
 const App = () => {
+  const [user] = useContext(UserContext)
   enableNotifications()
 
   const toggleTimerView = (hide) => {
@@ -22,34 +26,39 @@ const App = () => {
   }
   
   return (
-    <section className="App">
-      <ToastContainer
-        position="top-center"
-        autoClose={10000}
-        closeOnClick
-        pauseOnHover={false}
-      />
-      <Header toggleTimerView={toggleTimerView} />
-      <HomeContainer />
-      <Switch>
-        <Route exact path="/" />
-        <Route
-          exact path="/settings"
-          render={() => <Settings toggleTimerView={toggleTimerView} />}
+    <>
+      {!user.token && <Login />}
+      {user.token &&
+      <section className="App">
+        <ToastContainer
+          position="top-center"
+          autoClose={10000}
+          closeOnClick
+          pauseOnHover={false}
         />
-        <Route
-          exact path="/about"
-          render={() => <About toggleTimerView={toggleTimerView} />}
-        />
-        <Route
-          exact path="/stats"
-          render={() => <Stats toggleTimerView={toggleTimerView} />}
-        />
-        <Route
-          render={() => <PageNotFound toggleTimerView={toggleTimerView} />}
-        />
-      </Switch>
-    </section>
+        <Header toggleTimerView={toggleTimerView} />
+        <HomeContainer />
+        <Switch>
+          <Route exact path="/" />
+          <Route
+            exact path="/settings"
+            render={() => <Settings toggleTimerView={toggleTimerView} />}
+          />
+          <Route
+            exact path="/about"
+            render={() => <About toggleTimerView={toggleTimerView} />}
+          />
+          <Route
+            exact path="/stats"
+            render={() => <Stats toggleTimerView={toggleTimerView} />}
+          />
+          <Route
+            render={() => <PageNotFound toggleTimerView={toggleTimerView} />}
+          />
+        </Switch>
+      </section>
+      }
+    </>
   );
 }
 
--- src/Components/HomeContainer/HomeContainer.js
+++ src/Components/HomeContainer/HomeContainer.js
@@ -1,31 +1,23 @@
 import React, { useContext } from 'react'
 import { ViewContext } from '../../Context/ViewContext'
-import { UserContext } from '../../Context/UserContext'
 import { VideoProvider } from '../../Context/VideoContext'
 import ContentDelivery from '../ContentDelivery/ContentDelivery'
 import CountdownTimer from '../CountdownTimer/CountdownTimer'
 import ContentSelection from '../ContentSelection/ContentSelection'
 import MoodRating from '../MoodRating/MoodRating'
-import Login from '../Login/Login'
 import style from './HomeContainer.module.scss'
 
 const HomeContainer = () => {
   const [ view ] = useContext(ViewContext)
-  const [ user ] = useContext(UserContext)
 
   return (
     <div className={style.HomeContainer} id="HomeContainer">
       <VideoProvider>
-        {!user.token && <Login />}
-        {user.token && (
-          <>
-            {view === "timer" && <CountdownTimer />}
-            {view === "content-selection" && <ContentSelection />}
-            {view === "content-delivery" && <ContentDelivery />}
-            {(view === "mood-rating-1" || view === "mood-rating-2") && (
-              <MoodRating />
-            )}
-          </>
+        {view === "timer" && <CountdownTimer />}
+        {view === "content-selection" && <ContentSelection />}
+        {view === "content-delivery" && <ContentDelivery />}
+        {(view === "mood-rating-1" || view === "mood-rating-2") && (
+          <MoodRating />
         )}
       </VideoProvider>
     </div>
