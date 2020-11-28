7dda48eee0973806e0e41253abcaf03e9afd86d4
Rachel Williams <rachelwilliams659@gmail.com>
2020-11-27 18:16:56 -0700

    Fix FocusModal tests by wrapping in appropriate providers

--- src/Components/FocusModal/FocusModal.test.js
+++ src/Components/FocusModal/FocusModal.test.js
@@ -3,17 +3,24 @@ import FocusModal from './FocusModal'
 import { render, waitFor } from '@testing-library/react'
 import { ViewProvider } from '../../Context/ViewContext'
 import { SessionProvider } from '../../Context/SessionContext'
+import { SettingsProvider } from '../../Context/SettingsContext'
+
+import { UserProvider } from '../../Context/UserContext'
 import { postSession } from '../../apiCalls'
 jest.mock('../../apiCalls')
 
 describe('FocusModal', () => {
   it('Should have the correct content on load', async () => {
     const { findByRole } = render(
-      <ViewProvider>
-        <SessionProvider>
-          <FocusModal />
-        </SessionProvider>
-      </ViewProvider>
+      <SettingsProvider>
+        <UserProvider>
+          <ViewProvider>
+            <SessionProvider>
+              <FocusModal />
+            </SessionProvider>
+          </ViewProvider>
+        </UserProvider>
+      </SettingsProvider>
     )
 
     const modalText = await findByRole('heading', { name: /get ready to focus/i })
@@ -23,11 +30,15 @@ describe('FocusModal', () => {
 
   it('Should make a network request when rendered', async () => {
     render(
-      <ViewProvider>
-        <SessionProvider>
-          <FocusModal />
-        </SessionProvider>
-      </ViewProvider>
+      <SettingsProvider>
+        <UserProvider>
+          <ViewProvider>
+            <SessionProvider>
+              <FocusModal />
+            </SessionProvider>
+          </ViewProvider>
+        </UserProvider>
+      </SettingsProvider>
     )
 
     await waitFor(() => expect(postSession).toBeCalled())
