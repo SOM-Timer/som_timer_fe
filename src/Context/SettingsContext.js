2fad709d39d796227a56e3711cb0ab33f6dbd49f
Rachel Williams <rachelwilliams659@gmail.com>
2020-11-27 17:34:52 -0700

    Remove useEffect hook to setSettings on load from SettingsContext

--- src/Context/SettingsContext.js
+++ src/Context/SettingsContext.js
@@ -1,26 +1,9 @@
-import React, { createContext, useState, useEffect } from 'react'
-import { getSettings } from '../apiCalls'
+import React, { createContext, useState } from 'react'
 
 export const SettingsContext = createContext();
 
 export const SettingsProvider = ({ children }) => {
   const [settings, setSettings] = useState({ workInterval: '25', breakInterval: '5', sound: 'reverbSplash', moodRating: true })
-
-  useEffect(() => {
-    getSettings()
-      .then(response => {
-        const workInterval = response.data.work_interval.split(':')[0]
-        const breakInterval = response.data.rest_interval.split(':')[0]
-        const sound = response.data.sound
-        const moodRating = response.data.mood
-        setSettings({ workInterval, breakInterval, sound, moodRating})
-      })
-      .catch(err => {
-        if (err.response) {
-          console.log(err.response.status)
-        }
-      })
-  }, [])
 
   return (
     <SettingsContext.Provider value={[ settings, setSettings ]}>
