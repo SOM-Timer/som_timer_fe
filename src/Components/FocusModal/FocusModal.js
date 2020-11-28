9f4a45d63aa4765fad13ec07f10823da8e3f7172
Rachel Williams <rachelwilliams659@gmail.com>
2020-11-27 18:01:08 -0700

    Add userId as argument to postSession in FocusModal

--- src/Components/FocusModal/FocusModal.js
+++ src/Components/FocusModal/FocusModal.js
@@ -1,16 +1,18 @@
 import React, { useContext, useEffect } from 'react'
 import { ViewContext } from '../../Context/ViewContext'
 import { SessionContext } from '../../Context/SessionContext'
+import { UserContext } from '../../Context/UserContext'
 import { postSession } from '../../apiCalls'
 import style from './FocusModal.module.scss'
 
 const FocusModal = () => {
   const setView = useContext(ViewContext)[1]
   const session = useContext(SessionContext)[0] 
+  const user = useContext(UserContext)[0]
 
   useEffect(() => {
     setTimeout(() => setView('timer'), 3000)
-    postSession(session)
+    postSession(session, user.userId)
   })
 
   return (
